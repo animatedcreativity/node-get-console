e3b5beaf7cddb1eff818c7fdf5edf077f5747923
Glitch (node-get-console) <none>
2019-01-10 09:47:45 +0000

    🔮🏗 Updated with Glitch

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const consoleData = "";
-const oldConsoleLog = console.log;
+var consoleData = "";
+var oldConsoleLog = console.log;
 console.log = function(data) {
   if (consoleData != "") consoleData += "\n";
   consoleData += JSON.stringify(data);
   oldConsoleLog.apply(console, arguments);
 }
-const oldConsoleClear = console.clear;
+var oldConsoleClear = console.clear;
 console.clear = function() {
   consoleData = "";
   oldConsoleLog.apply(console, arguments);
